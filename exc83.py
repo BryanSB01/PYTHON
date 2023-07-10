76f6723b6f7a8679e26cd5c5805c9bd15f939851
Bryan Belum <95600131+BryanSB01@users.noreply.github.com>
2023-07-10 16:32:39 -0300

    Delete exc83.py

--- exc83.py
+++
@@ -1,7 +0,0 @@
-exp = str(input("Digite uma expressão matemática. "))
-abr = exp.count("(")
-fcd = exp.count(")")
-if abr == fcd:
-    print("Sua expressão está correta!")
-elif abr != fcd:
-    print("Sua expressão está incorreta!")
