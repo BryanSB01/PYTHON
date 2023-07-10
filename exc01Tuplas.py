5b75d262b5aaee0071b7cd6e1d0db1fef4c13ea3
Bryan Belum <95600131+BryanSB01@users.noreply.github.com>
2023-07-10 16:26:51 -0300

    Delete exc01Tuplas.py

--- exc01Tuplas.py
+++
@@ -1,8 +0,0 @@
-numeros = ('zero','um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
-num = int(input('Digite um número de 0 a 20\n'))
-while num < 0 or num > 20:
-    print("Número fora do intervalo. Tente novamente.")
-    num = int(input('Digite um número de 0 a 20\n'))    
-for pos, n in enumerate(numeros):
-    if num == pos:
-        print(f"Você digitou o número: {n}")
