c3c1b820392b89ba9e8f6d7993878da2b645ad25
Bryan Belum <95600131+BryanSB01@users.noreply.github.com>
2023-07-10 16:27:50 -0300

    Delete exc03Tuplas.py

--- exc03Tuplas.py
+++
@@ -1,5 +0,0 @@
-from random import randrange
-numeros = (randrange(10), randrange(10), randrange(10), randrange(10), randrange(10))
-print(f"Os valores sorteados foram {numeros}")
-print(f"O maior valor sorteado foi {max(numeros)}")
-print(f"O menor valor sorteado foi {min(numeros)}")
