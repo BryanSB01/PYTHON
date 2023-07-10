468277c466d87717639c750084f75eac5bcd663b
Bryan Belum <95600131+BryanSB01@users.noreply.github.com>
2023-07-10 16:33:31 -0300

    Delete tuplas01.py

--- tuplas01.py
+++
@@ -1,6 +0,0 @@
-sacola = "Banana", 15, True #Empacotamento de Tupla
-print(sacola)
-(fruta, idade, casado) = sacola #Desempacotamento de Tupla
-print(f"A fruta é: {fruta}")
-print(f"Ele é casado? {casado}")
-print(f"Qual a idade dele? {idade}")
